3b975b13e0fbe80b3e951fbb3000cc517b7ce488
Vasiliy Peshko <83081524+SPACE-DOGGO@users.noreply.github.com>
2022-11-20 16:44:50 +0200

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,247 @@
+#include <iostream>
+#include "function.h"
+using namespace std;
+
+void initInt(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+}
+
+
+void showInt(int arr[], int n) 
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+
+void findMinInt(int arr[], int n) 
+{
+    int min = arr[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min)
+        {
+            swap(arr[i], min);
+        }
+    }
+
+    cout << "Минимальное значение: " << min << "\n";
+}
+
+
+void findMaxInt(int arr[], int n) 
+{
+    int max = arr[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            swap(arr[i], max);
+        }
+    }
+
+    cout << "Максимальное значение: " << max << "\n";
+}
+
+
+void bSortInt(int arr[], int n) 
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] > arr[i])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
+
+void changeElInt(int arr[])
+{
+    int d, val;
+
+    cout << "Введите индекс элемента" << "\n";
+
+    cin >> d;
+
+    cout << "Введите новое значение" << "\n";
+
+    cin >> val;
+
+    arr[d + 1] = val;
+}
+
+void initFl(float arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = float(((float)rand() / RAND_MAX));;
+    }
+}
+
+
+void showFl(float arr[], int n) 
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+
+void findMinFl(float arr[], int n) 
+{
+    float min = arr[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min)
+        {
+            swap(arr[i], min);
+        }
+    }
+
+    cout << "Min: " << min << "\n";
+}
+
+
+void findMaxFl(float arr[], int n) 
+{
+    float max = arr[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            swap(arr[i], max);
+        }
+    }
+
+    cout << "Max: " << max << "\n";
+}
+
+
+void bSortFl(float arr[], int n) 
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
+
+void changeElFl(float arr[])
+{
+    int d;
+    float val;
+
+    cout << "Введите индекс элемента" << "\n";
+
+    cin >> d;
+
+    cout << "Введите новое значение" << "\n";
+
+    cin >> val;
+
+    arr[d + 1] = val;
+}
+
+
+void initCh(char arr[], int n) 
+{
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+}
+
+
+void showCh(char arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+
+void findMinCh(char arr[], int n)
+{
+    char min = arr[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min)
+        {
+            swap(arr[i], min);
+        }
+    }
+
+    cout << "Min: " << min << "\n";
+}
+
+
+void findMaxCh(char arr[], int n)
+{
+    char max = arr[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            swap(arr[i], max);
+        }
+    }
+
+    cout << "Max: " << max << "\n";
+}
+
+
+void bSortCh(char arr[], int n) 
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
+
+void changeElCh(char arr[])
+{
+    int d;
+
+    char val;
+
+    cout << "Введите индекс элемента" << "\n";
+
+    cin >> d;
+
+    cout << "Введите новое значение" << "\n";
+
+    cin >> val;
+
+    arr[d + 1] = val;
+}
---
+++ prog.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include "function.h"
+#define N 10
+using namespace std;
+
+int main() {
+
+    #define INTEGER
+
+    setlocale(0, "UKR");
+
+    int arr[N];
+
+    cout << "Массив #1:" << "\n";
+
+    init(arr, N);
+    show(arr, N);
+
+    cout << "\n\n";
+
+    cout << "Отсортированный масив: " << "\n";
+
+    bSort(arr, N);
+    show(arr, N);
+
+    cout << "\n\n";
+
+    findMin(arr, N);
+
+    cout << "\n";
+
+    findMaxInt(arr, N);
+
+    cout << "\n";
+
+    changeElInt(arr);
+
+#undef INTEGER
+
+#define FLOAT
+#include "function.h"
+
+    cout << "\n";
+
+    float arr1[N];
+
+    cout << "Массив #2:" << "\n";
+
+    init(arr1, N);
+    show(arr1, N);
+
+    cout << "\n\n";
+
+    cout << "Отсортированный масив: " << "\n";
+
+    bSort(arr1, N);
+    show(arr1, N);
+
+    cout << "\n\n";
+
+    findMin(arr1, N);
+
+    cout << "\n";
+
+    findMax(arr1, N);
+
+    cout << "\n";
+
+    changeEl(arr1);
+
+#undef FLOAT
+
+#define CHAR
+#include "function.h"
+
+    cout << "\n";
+
+    char arr2[N];
+
+    cout << "Массив #3:" << "\n";
+
+    init(arr2, N);
+    show(arr2, N);
+
+    cout << "\n\n";
+
+    cout << "Отсортированный масив: " << "\n";
+
+    bSort(arr2, N);
+    show(arr2, N);
+
+    cout << "\n\n";
+
+    findMin(arr2, N);
+
+    cout << "\n";
+
+    findMax(arr2, N);
+
+    cout << "\n";
+
+    changeEl(arr2);
+
+#undef CHAR
+
+    system("pause");
+    return 0;
+}
